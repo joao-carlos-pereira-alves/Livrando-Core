132179d2e6a74a4abf73599e48f44397cfef1bc2
João Carlos <joaocarlopa@gmail.com>
2023-07-11 14:07:05 -0300

    feat(#86786pmyd): created encryption and validations user data

--- db/migrate/20230702181234_devise_create_users.rb
+++ db/migrate/20230702181234_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email_ciphertext, null: false, default: ""
+      t.string :email_bidx
+      t.string :cpf_ciphertext, null: false, default: ""
+      t.string :cpf_bidx
       t.string :encrypted_password, null: false, default: ""
+
+      t.string :name
+      t.date   :birth_date
+      t.string :phone_ciphertext, null: false, default: ""
+      t.string :phone_bidx
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,7 +44,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email_bidx,           unique: true
+    add_index :users, :cpf_bidx,             unique: true
+    add_index :users, :phone_bidx,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
