70c95ff67686a5492fbaf7d92ad7bbfc8ada0dbe
jennysol <jennyssoliver@gmail.com>
2023-10-30 23:14:18 -0300

    fix: add skip before action to create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :set_user, only: %i[ show update destroy ]
+      skip_before_action :authenticate_user!, :only => :create
 
       # GET /users
       # GET /users.json
@@ -47,7 +48,7 @@ module Api
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_user
-          @user = User.find_by(uuid: params[:uuid])
+          @user = User.find(params[:id])
         end
 
         # Only allow a list of trusted parameters through.
